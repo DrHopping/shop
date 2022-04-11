020e8e20c5d09858cf43884de45a0cdcd2520ac8
Mykyta Pedorenko <mpedorenko@corelogic.com>
2022-04-11 21:32:23 +0300

    Add order-by pipe

--- src/app/products/components/product-list/product-list.component.ts
+++ src/app/products/components/product-list/product-list.component.ts
@@ -1,21 +1,31 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { CartService } from 'src/app/cart/services/cart.service';
 import { ProductModel } from '../../models/product.model';
 import { ProductsService } from '../../services/products.service';
-import { ProductComponent } from '../product/product.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-  constructor(public productService: ProductsService, private cartService: CartService) { }
+  sortOptions: Partial<Record<keyof ProductModel, string>> = {
+    name: 'Name',
+    price: 'Price',
+    isAvailable: 'Availability',
+  };
+
+  selected!: keyof ProductModel;
+  isAscending: boolean = false;
+
+  constructor(
+    public productService: ProductsService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {}
 
   onAddProductToCart(product: ProductModel) {
     this.cartService.addProduct(product);
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { HighlightDirective } from './directives/highlight.directive';
 import { BorderDirective } from './directives/border.directive';
+import { OrderByPipe } from './pipes/order-by.pipe';
+import { FormsModule } from '@angular/forms';
 
-const directives = [HighlightDirective, BorderDirective];
+const directives = [HighlightDirective, BorderDirective, OrderByPipe];
 
 const modules = [
   MatCardModule,
@@ -16,6 +20,9 @@ const modules = [
   MatDividerModule,
   MatListModule,
   MatIconModule,
+  MatSelectModule,
+  MatCheckboxModule, 
+  FormsModule
 ];
 
 @NgModule({
