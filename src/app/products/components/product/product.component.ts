76a26498f5cf4e65a7888432115550e757496908
Mykyta Pedorenko <mpedorenko@corelogic.com>
2022-05-31 20:33:01 +0300

    Use router facade navigation

--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -1,22 +1,27 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { RouterFacade } from 'src/app/store';
 import { ProductModel } from '../../models/product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
   @Input() product!: ProductModel;
-  @Output() addProductToCart: EventEmitter<ProductModel> = new EventEmitter<ProductModel>();
+  @Output() addProductToCart: EventEmitter<ProductModel> =
+    new EventEmitter<ProductModel>();
 
-  constructor() { }
+  constructor(private routerFacade: RouterFacade) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onBuyButtonClick(): void {
     this.addProductToCart.emit(this.product);
   }
 
+  onViewProduct(id: number) {
+    const link = ['/products', id];
+    this.routerFacade.goTo({ path: link });
+  }
 }
